a5998fe5fb37ceb3700d0f58ba84ab18efdd29e4
simplydesigner <simplydesigner@ya.ru>
2016-08-20 20:32:38 +0300

    Initial Commit

---
+++ threadsafe_queue/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  threadsafe_queue
+//
+//  Created by simplydesigner on 20/08/16.
+//  Copyright © 2016 JustDevelopers. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
